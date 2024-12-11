df693094de5bd236931dc2e49802b9063fab8bd8
sechan <sc6315@naver.com>
2024-12-11 10:17:41 +0900

    sechan

---
+++ counselProject/src/main/java/com/example/counselProject/CounselProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.counselProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounselProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounselProjectApplication.class, args);
+	}
+
+}
---
+++ counselProject/src/test/java/com/example/counselProject/CounselProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.counselProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounselProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
