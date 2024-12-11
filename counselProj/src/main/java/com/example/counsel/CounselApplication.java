ae18ae84c0f4e7e4798c20fe5f8538c1ad2e3748
고영1 <akflzk33@naver.com>
2024-12-11 16:14:59 +1030

    test1

--- counselProj/src/main/java/com/example/counsel/CounselApplication.java
+++ counselProj/src/main/java/com/example/counsel/CounselApplication.java
@@ -2,6 +2,7 @@ package com.example.counsel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CounselApplication {
@@ -9,5 +10,9 @@ public class CounselApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CounselApplication.class, args);
 	}
-
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
+}
 }
