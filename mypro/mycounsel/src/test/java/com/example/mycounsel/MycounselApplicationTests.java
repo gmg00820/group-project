07af8460f4764c58d0912f41dc4ae5053c9bc6a7
Kimjinma <164548545+Kimjinma@users.noreply.github.com>
2024-12-11 14:40:45 +1100

    Add files via upload

---
+++ mypro/mycounsel/src/test/java/com/example/mycounsel/MycounselApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycounsel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycounselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
