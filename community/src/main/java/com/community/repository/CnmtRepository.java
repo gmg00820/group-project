b4a3f889c3695fc76623aac804127d342303b2d8
영민 <92725024+youngmin2306@users.noreply.github.com>
2024-12-17 12:05:01 +0900

    Delete community directory

--- community/src/main/java/com/community/repository/CnmtRepository.java
+++
@@ -1,8 +0,0 @@
-package com.community.repository;
-
-import com.community.domain.CNMT;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CnmtRepository extends JpaRepository<CNMT, Long> {
-
-}
--- community/src/main/java/com/community/repository/PstRepository.java
+++
@@ -1,7 +0,0 @@
-package com.community.repository;
-
-import com.community.domain.PST;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PstRepository extends JpaRepository<PST, Long> {
-}
